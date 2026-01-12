48dd6307b1b980d744857be934b9631d1ee585cc
lonperman <lonperman@gmail.com>
2026-01-12 18:11:16 -0500

    feat: structure the routes project

---
+++ src/app.router.tsx
@@ -0,0 +1,79 @@
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router";
+import { HomePage } from "./shop/layouts/pages/home/HomePage";
+import { ShopLayout } from "./shop/layouts/ShopLayout";
+import { ProductPage } from "./shop/layouts/pages/product/ProductPage";
+import { GenderPage } from "./shop/layouts/pages/gender/GenderPage";
+import { LoginPage } from "./auth/pages/login/LoginPage";
+import { RegisterPage } from "./auth/pages/register/RegisterPage";
+import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
+import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
+
+const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
+const AdminLayout = lazy(() => import("./admin/AdminLayout"));
+
+
+export const appRouter = createBrowserRouter([
+    // Main routes
+    {
+        path: '/',
+        element: <ShopLayout/>,
+        children: [
+            {
+                index: true,
+                element: <HomePage/>
+            },
+            {
+                path: 'product/:slug',
+                element: <ProductPage/>
+            },
+            {
+                path: 'gender/:gender',
+                element: <GenderPage/>
+            }
+        ]
+    },
+    // Auth routes
+    {
+        path: '/auth',
+        element: <AuthLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Navigate to='/auth/login'/>
+            },
+            {
+                path: 'login',
+                element: <LoginPage/>
+            },
+            {
+                path: 'register',
+                element: <RegisterPage/>
+            }
+        ]
+    },
+    // Admin routes
+    {
+        path: '/admin',
+        element: <AdminLayout/>,
+        children: [
+            {
+                index: true,
+                element: <DashboardPage/>
+            },
+            {
+                path: 'products',
+                element: <AdminProductsPage/>
+            },
+            {
+                path: 'products/:id',
+                element: <AdminProductsPage/>
+            }
+        ]
+    },
+    // 404 - Not Found
+    {
+        path: '*',
+        element: <Navigate to='/'/>
+    }
+])

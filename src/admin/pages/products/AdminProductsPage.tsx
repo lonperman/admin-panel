c38e539e0cfdd6c6bbf772f6f62f764c6f39a2ef
lonperman <lonperman@gmail.com>
2026-02-01 23:37:29 -0500

    feat:Design Products Page

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { Link } from 'react-router'
+import { AdminTitle } from '@/admin/components/AdminTitle'
+import {Table,
+        TableBody,
+        TableCell,
+        TableHead,
+        TableHeader,
+        TableRow } from '@/components/ui/table'
+import { CustomPagination } from '@/components/custom/CustomPagination'
+import { PlusIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 export const AdminProductsPage = () => {
   return (
-    <div>AdminProductsPage</div>
+    <>
+      <div className='flex justify-between items-center'>
+        <AdminTitle
+          title='Productos'
+          subtitle='Aqui puedes ver y administrar los productos'
+        />
+
+        <Link to='/admin/products/new'>
+          <Button>
+            <PlusIcon />
+              Nuevo Producto
+          </Button>
+        </Link>
+      </div>
+
+      <Table className='
+        bg-white
+        p-10
+        shadow-xs
+        border
+        border-gray-200
+        mb-10
+      '>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Imagen</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Precio</TableHead>
+            <TableHead>Categoría</TableHead>
+            <TableHead>Inventario</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>
+              <img 
+                src='htttps://placehold.co/250x250'
+                alt='Product'
+                className='w-10 h-10 rounded-md object-cover'
+              />
+            </TableCell>
+            <TableCell>Producto 1</TableCell>
+            <TableCell>$250.00</TableCell>
+            <TableCell>Categoria 1</TableCell>
+            <TableCell>100 stock</TableCell>
+            <TableCell>XS, S, M, L, XL</TableCell>
+            <TableCell className="text-right">
+              <Link 
+                to={`/admin/products/t-shirt-teslo`}
+              >
+                Editar
+              </Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={10} />
+    </>
   )
 }
